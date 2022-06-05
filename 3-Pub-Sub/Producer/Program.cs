d7e3e6d5974a05553fde1d66d7fa7b1fc3d06b4f
astanchev <mr_stanchev@hotmail.com>
2022-06-05 18:39:26 +0300

    Competing consumers and Pub-Sub patterns

---
+++ 3-Pub-Sub/Producer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(
+    exchange: "pubsub",
+    type: ExchangeType.Fanout
+);
+
+var message = "Hello I want to broadcast this message!";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("pubsub", "", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
