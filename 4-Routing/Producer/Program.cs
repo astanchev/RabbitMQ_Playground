c5b602df286be98ee09ebc2ec9726208342629de
astanchev <mr_stanchev@hotmail.com>
2022-06-06 06:37:39 +0300

    Routing and topic exchange

---
+++ 4-Routing/Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "myroutingexchange",
+    ExchangeType.Direct
+);
+
+var message = "This message needs to be routed!";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(
+    exchange: "myroutingexchange",
+    routingKey: "both",
+    //routingKey: "analyticsonly",
+    //routingKey: "paymentsonly",
+    basicProperties: null,
+    body: encodedMessage
+);
+
+Console.WriteLine($"Published message: {message}");
---
+++ 5-Topics/Producer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "mytopicexchange",
+    ExchangeType.Topic
+);
+
+var userPaymentsMessage = "An european user paid for something!";
+
+var userPaymentsBody = Encoding.UTF8.GetBytes(userPaymentsMessage);
+
+channel.BasicPublish(
+    exchange: "mytopicexchange",
+    routingKey: "user.europe.payments",
+    basicProperties: null,
+    body: userPaymentsBody
+);
+
+Console.WriteLine($"Published message: {userPaymentsMessage}");
+
+
+var businessOrderMessage = "An european business ordered goods!";
+
+var businessOrderBody = Encoding.UTF8.GetBytes(businessOrderMessage);
+
+channel.BasicPublish(
+    exchange: "mytopicexchange",
+    routingKey: "business.europe.orders",
+    basicProperties: null,
+    body: businessOrderBody
+);
+
+Console.WriteLine($"Published message: {businessOrderMessage}");
