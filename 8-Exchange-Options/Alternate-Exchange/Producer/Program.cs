c9276ac25547bf6fae1a75a192524fd9f205af03
astanchev <mr_stanchev@hotmail.com>
2022-08-10 10:33:28 +0300

    Add Exchange Options

---
+++ 8-Exchange-Options/Alternate-Exchange/Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+
+channel.ExchangeDeclare(
+    exchange: "altexchange",
+    type: ExchangeType.Fanout);
+
+channel.ExchangeDeclare(
+    exchange: "mainexchange",
+    type: ExchangeType.Direct,
+    arguments: new Dictionary<string, object>{
+        {"alternate-exchange", "altexchange"}
+});
+
+var message = "This is my first Message";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+//channel.BasicPublish("mainexchange", "test", null, body); //goes to main queue
+channel.BasicPublish("mainexchange", "test2", null, body); // goes to alt queue because of the wrong key
+
+Console.WriteLine($"Send message: {message}");
